325200f3cb54e03825e2c1eec2c9c8bf6ab164bc
Juan <makaujoseph8@gmai.com>
2025-07-25 00:54:56 +0300

    create eks module

---
+++ eks/6-variables.tf
@@ -0,0 +1,45 @@
+variable "env" {
+    description = "Environment name"
+    type = string
+  
+}
+
+variable "eks_name" {
+    description = "Name of the cluster"
+    type = string
+  
+}
+
+variable "eks_version" {
+    description = "Desired Kubernetes version for the master"
+  
+}
+ variable "subnet_ids" {
+    description = "List of subnets IDS, must be in at least two different availability zones"
+    type = list(string)
+   
+ }
+
+ variable "node_iam_policies" {
+    description = "List of IAM policies to attach to EKS-managed nodes"
+    default = {
+        1 = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+        2 = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+        3 = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+        4 = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+    }
+   
+ }
+
+ variable "node_groups" {
+    description = "EKS node groups"
+    type = map(any)
+   
+ }
+
+ variable "enable_irsa" {
+    description = "Dertimines whether to create an OpenID Connect provider for EKS"
+    type = bool
+    default = true 
+   
+ }
