9b785da89b2bf10b0ca37665043527853bba6ee6
Juan <makaujoseph8@gmai.com>
2025-08-07 21:49:32 +0300

    Fixed:Code dependency

--- kubernetes-addons/1-cluster-autoscaler.tf
+++ kubernetes-addons/1-cluster-autoscaler.tf
@@ -21,9 +21,8 @@ data "aws_iam_policy_document" "cluster_autoscaler" {
 }
 
 resource "aws_iam_role" "cluster_autoscaler" {
-  count = var.enable_cluster_autoscaler ? 1 : 0
-
-  assume_role_policy = data.aws_iam_policy_document.cluster_autoscaler.json
+  count              = var.enable_cluster_autoscaler && var.openid_provider_arn != null && var.openid_provider_arn != "" ? 1 : 0
+  assume_role_policy = data.aws_iam_policy_document.cluster_autoscaler[0].json # ✅ Note the [0]
   name               = "${var.eks_name}-cluster-autoscaler"
 }
 
