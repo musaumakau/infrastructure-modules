12a4b6d93d819e53cd12a5bf639e817e55d55fd5
Juan <makaujoseph8@gmai.com>
2025-08-07 18:52:19 +0300

    Format code

--- kubernetes-addons/1-cluster-autoscaler.tf
+++ kubernetes-addons/1-cluster-autoscaler.tf
@@ -4,7 +4,7 @@ locals {
 
 data "aws_iam_openid_connect_provider" "this" {
   count = local.oidc_exists ? 1 : 0
-  arn = var.openid_provider_arn
+  arn   = var.openid_provider_arn
 
 }
 
@@ -15,7 +15,7 @@ data "aws_iam_policy_document" "cluster_autoscaler" {
 
     condition {
       test     = "StringEquals"
-        variable = "${replace(data.aws_iam_openid_connect_provider.this[0].url, "https://", "")}:sub"
+      variable = "${replace(data.aws_iam_openid_connect_provider.this[0].url, "https://", "")}:sub"
       values   = ["system:serviceaccount:kube-system:cluster-autoscaler"]
     }
 
