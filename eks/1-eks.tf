d578d23cbc7a3c4b6cd3b160d9061eeff2f196fb
Juan <makaujoseph8@gmai.com>
2025-08-09 21:51:28 +0300

    Add user arn

--- eks/1-eks.tf
+++ eks/1-eks.tf
@@ -96,12 +96,24 @@ resource "aws_eks_cluster" "this" {
   depends_on = [aws_iam_role_policy_attachment.eks]
 }
 resource "aws_eks_access_entry" "local_admin" {
-  cluster_name      = aws_eks_cluster.this.name
-  principal_arn     = "arn:aws:iam::649203810550:user/Kay"
-  type              = "STANDARD"
-  kubernetes_groups = ["system:masters"]
+  cluster_name  = aws_eks_cluster.this.name
+  principal_arn = "arn:aws:iam::649203810550:user/Kay"
+  type          = "STANDARD"
+
 
   tags = {
     Name = "${var.env}-${var.eks_name}-local-admin-access"
   }
+}
+
+resource "aws_eks_access_policy_association" "local_admin" {
+  cluster_name  = aws_eks_cluster.this.name
+  principal_arn = "arn:aws:iam::649203810550:user/Kay"
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+
+  depends_on = [aws_eks_access_entry.local_admin]
 }
