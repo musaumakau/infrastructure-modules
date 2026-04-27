de7788f830a6dea16a4bb5587ee7b7a13685cd81
musaumakau <samwuel@Devops.local>
2026-04-27 14:15:33 +0100

    remove numbering

---
+++ kubernetes-addons/variables.tf
@@ -0,0 +1,45 @@
+variable "openid_provider_arn" {
+  description = "ARN of the OIDC provider for the EKS cluster"
+  type        = string
+  default     = null
+}
+
+variable "cluster_oidc_issuer_url" {
+  description = "The URL on the EKS cluster OIDC Issuer"
+  type        = string
+}
+
+variable "eks_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
+
+variable "enable_cluster_autoscaler" {
+  description = "Enable cluster autoscaler"
+  type        = bool
+  default     = true
+}
+
+variable "skip_helm_deployments" {
+  description = "Skip helm deployments"
+  type        = bool
+  default     = false
+}
+
+variable "cluster_autoscaler_helm_version" {
+  description = "Helm chart version for cluster autoscaler"
+  type        = string
+
+}
+
+variable "enable_aws_lbc" {
+  description = "Enable AWS Load Balancer Controller"
+  type        = bool
+  default     = false
+}
+
+variable "aws_lbc_helm_version" {
+  description = "Helm chart version for AWS Load Balancer Controller"
+  type        = string
+  default     = "1.7.1"
+}
