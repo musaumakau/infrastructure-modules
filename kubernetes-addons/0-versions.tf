a2fe95e7f502accacb7c849917051fb2251ab874
Juan Makau <46565239+musaumakau@users.noreply.github.com>
2026-04-28 20:29:17 +0300

    feat(kubernetes-addons): add gp3 StorageClass and kubernetes provider (#48)
    Co-authored-by: musaumakau <samwuel@Devops.local>

--- kubernetes-addons/0-versions.tf
+++ kubernetes-addons/0-versions.tf
@@ -1,15 +1,17 @@
 terraform {
   required_version = ">= 1.0.0, < 2.0.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.40.0"
     }
-
     helm = {
       source  = "hashicorp/helm"
       version = "~> 2.9"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.27"
     }
   }
 }
