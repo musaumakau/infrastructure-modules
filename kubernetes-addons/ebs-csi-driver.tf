6fccc216d0ea62cc6fbd9a32081f31a25ed8644f
musaumakau <samwuel@Devops.local>
2026-04-28 18:25:53 +0100

    feat(kubernetes-addons): add gp3 StorageClass and kubernetes provider

--- kubernetes-addons/ebs-csi-driver.tf
+++ kubernetes-addons/ebs-csi-driver.tf
@@ -48,3 +48,24 @@ resource "aws_eks_addon" "ebs_csi" {
     "eks_addon" = "ebs-csi-driver"
   }
 }
+
+resource "kubernetes_storage_class" "gp3" {
+  count = var.enable_ebs_csi_driver ? 1 : 0
+
+  metadata {
+    name = "gp3"
+    annotations = {
+      "storageclass.kubernetes.io/is-default-class" = "true"
+    }
+  }
+
+  storage_provisioner    = "ebs.csi.aws.com"
+  volume_binding_mode    = "WaitForFirstConsumer"
+  allow_volume_expansion = true
+
+  parameters = {
+    type = "gp3"
+  }
+
+  depends_on = [aws_eks_addon.ebs_csi]
+}
