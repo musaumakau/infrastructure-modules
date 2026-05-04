584a92c02026986ec37489fd164708eb3d4c3ae7
Juan Makau <46565239+musaumakau@users.noreply.github.com>
2026-05-04 16:58:47 +0300

    Feature/kubernetes addons v0.1.0 (#50)

---
+++ kubernetes-addons/keda.tf
@@ -0,0 +1,95 @@
+data "aws_iam_policy_document" "keda" {
+  count = var.enable_keda && var.openid_provider_arn != null && var.openid_provider_arn != "" ? 1 : 0
+
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(var.cluster_oidc_issuer_url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:keda:keda-operator"]
+    }
+
+    principals {
+      identifiers = [var.openid_provider_arn]
+      type        = "Federated"
+    }
+  }
+}
+
+resource "aws_iam_role" "keda" {
+  count              = var.enable_keda && var.openid_provider_arn != null && var.openid_provider_arn != "" ? 1 : 0
+  name               = "${var.eks_name}-keda"
+  assume_role_policy = data.aws_iam_policy_document.keda[0].json
+
+  tags = {
+    "eks_addon" = "keda"
+  }
+}
+
+resource "aws_iam_policy" "keda" {
+  count = var.enable_keda && var.openid_provider_arn != null && var.openid_provider_arn != "" ? 1 : 0
+  name  = "${var.eks_name}-keda"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "sqs:GetQueueAttributes",
+          "sqs:GetQueueUrl",
+          "sqs:ListQueues",
+          "sqs:ListDeadLetterSourceQueues",
+          "sqs:ReceiveMessage",
+          "sqs:DeleteMessage",
+        ]
+        Resource = "arn:aws:sqs:${var.aws_region}:${var.aws_account_id}:*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "cloudwatch:GetMetricStatistics",
+          "cloudwatch:ListMetrics",
+          "cloudwatch:GetMetricData",
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+
+  tags = {
+    "eks_addon"                = "keda"
+    "checkov:skip=CKV_AWS_290" = "KEDA requires CloudWatch read access for metric-based scaling"
+    "checkov:skip=CKV_AWS_355" = "CloudWatch metrics require wildcard resource as they are not resource-specific"
+  }
+}
+resource "aws_iam_role_policy_attachment" "keda" {
+  count      = var.enable_keda && var.openid_provider_arn != null && var.openid_provider_arn != "" ? 1 : 0
+  role       = aws_iam_role.keda[0].name
+  policy_arn = aws_iam_policy.keda[0].arn
+}
+
+resource "helm_release" "keda" {
+  count = var.skip_helm_deployments || !var.enable_keda || var.openid_provider_arn == null || var.openid_provider_arn == "" ? 0 : 1
+
+  name             = "keda"
+  repository       = "https://kedacore.github.io/charts"
+  chart            = "keda"
+  namespace        = "keda"
+  version          = var.keda_helm_version
+  create_namespace = true
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = aws_iam_role.keda[0].arn
+  }
+
+  set {
+    name  = "podIdentity.aws.irsa.enabled"
+    value = "true"
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.keda, helm_release.aws_lbc]
+}
